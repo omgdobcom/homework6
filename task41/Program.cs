08a5abc984c1130e76a4739c6504348ff6a85a49
omg.dobcom <omg.dobcom@gmail.com>
2022-12-02 18:56:17 +0300

    homework6 done

---
+++ task41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int PositiveNum(int[] arr)
+{
+int count = 0;
+ 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine($"Количество чисел больше нуля {PositiveNum(arr)}");
---
+++ task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43:  Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите  значение точки b1:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  значение точки k1:");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  значение точки b2:");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  значение точки k2:");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double FindX(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k1 - k2);
+    return x;
+} 
+
+double FindY(double b1, double k1)
+{
+    double x = FindX(b1,k1,b2,k2);
+    double y = k1 * x + b1;
+    return x;
+}
+
+double x = FindX(b1, k1, b2, k2);
+double y = FindY(b1, k1);
+
+Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
